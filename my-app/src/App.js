885f0eb7f15a141757987de504eb21acd1f72193
AntonPolskij <polskij.anton707@gmail.com>
2022-03-11 22:28:42 +0300

    template with grid

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { Technologies } from './Technologies';
-import { Header } from './Header';
-import { Footer } from './Footer';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg" width="50px" height="50px"></img>
+      </header>
+      <nav className='nav'>
+        <ul>
+          <li>Profile</li>
+          <li>Messages</li>
+          <li>News</li>
+          <li>Music</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+        </div>
+        <div>
+          va+descr
+        </div>
+        <div>
+          myPosts
+          <div>
+            New Post
+          </div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
     </div>
   );
 }
--- my-app/src/Footer.js
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-    return (
-        <div>
-            Anton.Polskiy Footer
-        </div>
-    );
-}
