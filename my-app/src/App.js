44bac524c38a83f6e886324bea8c89f080bfe3e7
AntonPolskij <polskij.anton707@gmail.com>
2022-03-11 21:40:15 +0300

    create-react-app

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <img src={logo}></img>
+      <ul>
+        <li>Raz</li>
+        <li>Dva</li>
+        <li>Tri</li>
+        <li>Xyz</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App;
